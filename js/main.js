a0e28c90bd24c388dc6af8ed2703bc7676a4d796
sasabrina <sabrinaealvarez@gmail.com>
2019-08-27 02:37:57 -0300

    category fetch renderizado sin esilos

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const api_key = 'e3087953b023a4a056fc42d81ebd595d';
+
+const categoryFetch = (container, category) => {
+    return fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${api_key}`)
+    .then(response => response.json())
+    .then(response => {
+        let arr = response.results.slice(0, 5);
+        printMovies(container, arr);
+    })
+}
+
+const initialize = () => {
+    categoryFetch('popular_wraper', 'popular')
+    categoryFetch('toprated_wraper', 'top_rated')
+    categoryFetch('upcoming_wraper', 'upcoming')
+    categoryFetch('nowplaying_wraper', 'now_playing')
+}
+
+const printMovies = (wraper, arr) => {
+    console.log(arr);
+    
+    const category_container = document.getElementById(wraper)
+    category_container.innerHTML = '';
+
+    const movies_container = document.createElement('ul')
+    movies_container.classList.add('movies_container');
+
+    arr.map(m => createMovieBox(movies_container, m))
+
+    category_container.appendChild(movies_container)
+}
+
+const createMovieBox = (container, movie) => {
+    const {id, poster_path, title} = movie;
+
+    const li = document.createElement('li')
+    li.classList.add('movie_box')
+
+    const a = document.createElement('a')
+    a.href = '#'
+    a.onclick = () => modalFnc;
+
+    const img = document.createElement('img')
+    img.src = `https://image.tmdb.org/t/p/w500/${poster_path}`
+
+    const figure = document.createElement('figure')
+    figure.appendChild(img)
+    a.appendChild(figure)
+
+    const movie_title = document.createElement('p')
+    movie_title.innerText = title
+    a.appendChild(movie_title)
+
+    li.appendChild(a)
+    container.appendChild(li)
+}
+
+
